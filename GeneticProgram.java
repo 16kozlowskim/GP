a63f397e2fc871d14c9f279e7409aa892aa122ec
16kozlowskim <michal.kozlowski2@gmail.com>
2017-12-27 19:54:31 +0100

    df

--- GeneticProgram.java
+++ GeneticProgram.java
@@ -4,6 +4,7 @@ public class GeneticProgram {
   static int maxGen;
   static int popSize;
 
+
   public static void initializeRobot(int popSize, int maxGen) {
     robotBatch = new Robot[popSize];
     this.maxGen = maxGen;
@@ -17,13 +18,19 @@ public class GeneticProgram {
   public static void main(String[] args) {
     initializeRobot(parseInt(args[0]), parseInt(args[1]));
 
+    String[] robotNames = new String[popSize];
+
     BattleRunner battle = new BattleRunner();
 
     for (int genCount = 1; genCount <= maxGen; genCount++) {
 
       for (int i = 0; i < popSize; i++) {
         robotBatch[i].createFile();
+        robotNames[i] = robotBatch[i].name;
       }
+      double[] fitnesses = battle.createBattle(robotNames);
+
+
 
 
     }
--- Robot.java
+++ Robot.java
@@ -13,12 +13,15 @@ public class Robot {
   String name;
   int ID;
   ArrayList<ArrayList<ExpressionNode>> tree;
+  Robot child;
 
   static final double
     crossTermProb = 0.1,
     crossFuncProb = 0.9,
     mutateTermProb = 0.2,
-    mutateFuncProb = 0.8;
+    mutateFuncProb = 0.8,
+    mutationProb = 0.05,
+    crossOverProb = 0.95;
 
   public Robot(int ID, int generation) {
     root = new ExpressionNode[genFunctNum];
@@ -35,91 +38,96 @@ public class Robot {
     }
   }
 
-  public Robot crossOver(Robot robot, int ID) {
-    Robot child = new Robot(ID, generation + 1);
+  public Robot geneticOp(Robot robot, int ID) {
+    child = new Robot(ID, generation + 1);
 
     for (int i = 0; i < root.length; i++) {
-      child.root[i] = root[i].copy();
-      child.tree.add(child.root[i].assemble(child.root[i]));
-      robot.tree.add(robot.root[i].assemble(robot.root[i]));
-
-      Boolean isTerminal1 = rng.nextDouble() < crossTermProb ? true : false;
-      Boolean isTerminal2 = rng.nextDouble() < crossTermProb ? true : false;
-
-      ExpressionNode a = child.root[i].getSubTree(isTerminal1);
-      ExpressionNode b;
-
-      do {
-        b = robot.root[i].getSubTree(isTerminal2);
-      } while (2*a.treeSize() + 1 < b.treeSize());
-
-      ExpressionNode replacement = b.copy();
-
-      int j;
-      for (j = 0; j < child.tree.get(i).size(); j++) {
-        if (child.tree.get(i).get(j) == a) break;
-      }
-      if (j == 0) {
-        child.root[i] = replacement;
-        System.out.println("Crossed Root at "+i);
-      } else {
-
-        int depth = child.tree.get(i).get(j).depth;
-
-        for (; j > 0; --j) {
-          if (child.tree.get(i).get(j).depth == depth - 1) break;
-        }
-
-        for (int n = 0; n < child.tree.get(i).get(j).arity; n++) {
-          if (child.tree.get(i).get(j).children[n] == a) {
-            child.tree.get(i).get(j).children[n] = replacement;
-            break;
-          }
-        }
-        replacement.fixDepths(child.tree.get(i).get(j).depth - replacement.depth + 1);
-      }
-      replacement.fixDepths(replacement.depth);
+      if (rng.nextDouble() < crossOverProb)
+        crossOver(robot, i);
+      else
+        mutate(i);
     }
+    
     return child;
   }
 
-  public Robot mutate(int ID) {
-    Robot child = new Robot(ID, generation + 1);
+  public void crossOver(Robot robot, int i) {
 
-    for (int i = 0; i < root.length; i++) {
-      child.root[i] = root[i].copy();
-      child.tree.add(child.root[i].assemble(child.root[i]));
+    child.root[i] = root[i].copy();
+    child.tree.add(child.root[i].assemble(child.root[i]));
+    robot.tree.add(robot.root[i].assemble(robot.root[i]));
 
-      Boolean isTerminal = rng.nextDouble() < mutateTermProb ? true : false;
+    Boolean isTerminal1 = rng.nextDouble() < crossTermProb ? true : false;
+    Boolean isTerminal2 = rng.nextDouble() < crossTermProb ? true : false;
 
-      ExpressionNode a = child.root[i].getSubTree(isTerminal);
+    ExpressionNode a = child.root[i].getSubTree(isTerminal1);
+    ExpressionNode b;
 
-      int j;
-      for (j = 0; j < child.tree.get(i).size(); j++) {
-        if (child.tree.get(i).get(j) == a) break;
+    do {
+      b = robot.root[i].getSubTree(isTerminal2);
+    } while (2*a.treeSize() + 1 < b.treeSize());
+
+    ExpressionNode replacement = b.copy();
+
+    int j;
+    for (j = 0; j < child.tree.get(i).size(); j++) {
+      if (child.tree.get(i).get(j) == a) break;
+    }
+    if (j == 0) {
+      child.root[i] = replacement;
+      System.out.println("Crossed Root at "+i);
+    } else {
+
+      int depth = child.tree.get(i).get(j).depth;
+
+      for (; j > 0; --j) {
+        if (child.tree.get(i).get(j).depth == depth - 1) break;
       }
 
-      if (j == 0) {
-        child.root[i] = new ExpressionNode(0);
-        child.root[i].evolve(0);
-      } else {
-
-        int depth = child.tree.get(i).get(j).depth;
-
-        for (; j > 0; --j) {
-          if (child.tree.get(i).get(j).depth == depth - 1) break;
+      for (int n = 0; n < child.tree.get(i).get(j).arity; n++) {
+        if (child.tree.get(i).get(j).children[n] == a) {
+          child.tree.get(i).get(j).children[n] = replacement;
+          break;
         }
+      }
+      replacement.fixDepths(child.tree.get(i).get(j).depth - replacement.depth + 1);
+    }
+    replacement.fixDepths(replacement.depth);
+  }
 
-        for (int n = 0; n < child.tree.get(i).get(j).arity; n++) {
-          if (child.tree.get(i).get(j).children[n] == a) {
-            child.tree.get(i).get(j).children[n] = new ExpressionNode(child.tree.get(i).get(j).depth + 1);
-            child.tree.get(i).get(j).children[n].evolve(child.tree.get(i).get(j).depth + 1);
-            break;
-          }
+  public void mutate(int i) {
+
+    child.root[i] = root[i].copy();
+    child.tree.add(child.root[i].assemble(child.root[i]));
+
+    Boolean isTerminal = rng.nextDouble() < mutateTermProb ? true : false;
+
+    ExpressionNode a = child.root[i].getSubTree(isTerminal);
+
+    int j;
+    for (j = 0; j < child.tree.get(i).size(); j++) {
+      if (child.tree.get(i).get(j) == a) break;
+    }
+
+    if (j == 0) {
+      child.root[i] = new ExpressionNode(0);
+      child.root[i].evolve(0);
+    } else {
+
+      int depth = child.tree.get(i).get(j).depth;
+
+      for (; j > 0; --j) {
+        if (child.tree.get(i).get(j).depth == depth - 1) break;
+      }
+
+      for (int n = 0; n < child.tree.get(i).get(j).arity; n++) {
+        if (child.tree.get(i).get(j).children[n] == a) {
+          child.tree.get(i).get(j).children[n] = new ExpressionNode(child.tree.get(i).get(j).depth + 1);
+          child.tree.get(i).get(j).children[n].evolve(child.tree.get(i).get(j).depth + 1);
+          break;
         }
       }
     }
-    return child;
   }
 
   public String createSourceCode() {
